eccea13221f9c60174aafaa8f485cda0c76f6b2b
Ivan Wang <113294786+Ivan-Wang-J-W@users.noreply.github.com>
2023-12-24 00:23:07 +0800

    reorg

--- lua/core/plugin_config/cmp.lua
+++ lua/core/plugin_config/cmp.lua
@@ -1,19 +1,41 @@
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
-local cmp = require('cmp')
-local luasnip = require 'luasnip'
+local  cmp = require("cmp")
+local  luasnip = require ("luasnip")
+local cmp_action = require('lsp-zero').cmp_action()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup{
+ luasnip.config.setup {}
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      with_text = true,
+      menu = {
+        buffer ="[buf]",
+        nvim_lua = "[api]",
+        nvim_lsp = "[LSP]",
+        path = "[path]",
+        luasnip = "[snip]",
+      },
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  },
+}
+cmp.setup {
   snippet = {
     expand = function(args)
-     require("luasnip").lsp_expand(args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
-  
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -43,30 +65,31 @@ cmp.setup{
       end
     end, { 'i', 's' }),
   },
+  -- where completions are sourced, order will affect output
   sources = {
-    { name = 'nvim_lua' },
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'luasnip' },
-    { name = 'buffer', keyword_length = 5 },
-    
-  },
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer",  keyword_length = 7 },
+    { name = "path" },
+
+  }
 }
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
-  
-  })
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
   })
+})
