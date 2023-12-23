168f7fef546f9d1e5f197e55d60321a2ed29d008
Ivan Wang <113294786+Ivan-Wang-J-W@users.noreply.github.com>
2023-12-24 00:24:06 +0800

    lazy loaded lsp-zero

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
 	},
 	{
 		'sainnhe/everforest',
-		},
+	},
 	--telescope plugin
 	{
 		'nvim-treesitter/nvim-treesitter',
@@ -101,29 +101,28 @@ require("lazy").setup({
 	},
 	-- NOTE: plugins related to LSPnoti
 	--  The configuration is done below. Search for lspconfig to find it below.
-
 	{
-		-- LSP Configuration & Plugins
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			{ 'williamboman/mason.nvim', config = true },
-			'williamboman/mason-lspconfig.nvim',
-			'neovim/nvim-lspconfig',
-
-			-- Useful status updates for LSP, bottom right
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-
-			-- Additional lua configuration, makes nvim stuff amazing!
-			'folke/neodev.nvim',
+		{
+			'VonHeikemen/lsp-zero.nvim',
+			branch = 'v3.x',
+			lazy = true,
+			config = false,
+			init = function()
+				-- Disable automatic setup, we are doing it manually
+				vim.g.lsp_zero_extend_cmp = 0
+				vim.g.lsp_zero_extend_lspconfig = 0
+			end,
 		},
-	},
+		{
+			'williamboman/mason.nvim',
+			lazy = false,
+			config = true,
+		},
 
-
-	{
 		-- Autocompletion
+		{
 		'hrsh7th/nvim-cmp',
+			event='InsertEnter',
 		dependencies = {
 			-- Snippet Engine & its associated nvim-cmp source
 			'L3MON4D3/LuaSnip',
@@ -140,6 +139,87 @@ require("lazy").setup({
 			'rafamadriz/friendly-snippets',
 		},
 	},
+		--[[	config = function()
+				-- Here is where you configure the autocompletion settings.
+				local lsp_zero = require('lsp-zero')
+				lsp_zero.extend_cmp()
+
+				-- And you can configure cmp even more, if you want to.
+				local cmp = require('cmp')
+				local cmp_action = lsp_zero.cmp_action()
+
+				cmp.setup({
+					formatting = lsp_zero.cmp_format(),
+					mapping = cmp.mapping.preset.insert({
+						['<C-Space>'] = cmp.mapping.complete(),
+						['<C-u>'] = cmp.mapping.scroll_docs(-4),
+						['<C-d>'] = cmp.mapping.scroll_docs(4),
+						['<C-f>'] = cmp_action.luasnip_jump_forward(),
+						['<C-b>'] = cmp_action.luasnip_jump_backward(),
+					})
+				})
+			end--]]
+		},
+
+		-- LSP
+		{
+			'neovim/nvim-lspconfig',
+			cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+			event = { 'BufReadPre', 'BufNewFile' },
+			dependencies = {
+				{ 'hrsh7th/cmp-nvim-lsp' },
+				{ 'williamboman/mason-lspconfig.nvim' },
+			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+			},
+			config = function()
+				-- This is where all the LSP shenanigans will live
+				local lsp_zero = require('lsp-zero')
+				lsp_zero.extend_lspconfig()
+
+				lsp_zero.on_attach(function(client, bufnr)
+					-- see :help lsp-zero-keybindings
+					-- to learn the available actions
+					lsp_zero.default_keymaps({ buffer = bufnr })
+				end)
+
+				require('mason-lspconfig').setup({
+					ensure_installed = {},
+					handlers = {
+						lsp_zero.default_setup,
+						lua_ls = function()
+							-- (Optional) Configure lua language server for neovim
+							local lua_opts = lsp_zero.nvim_lua_ls()
+							require('lspconfig').lua_ls.setup(lua_opts)
+						end,
+					}
+				})
+			end
+		},
+	
+	--[[{
+		-- LSP Configuration & Plugins
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason-lspconfig.nvim',
+			'neovim/nvim-lspconfig',
+
+			-- Useful status updates for LSP, bottom right
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		},
+	},
+--]]
+
+	--pictogram for lsp
+	{ 'onsails/lspkind.nvim' },
 	-- Useful plugin to show you pending keybinds.
 	{ 'folke/which-key.nvim', opts = {} },
 	{
