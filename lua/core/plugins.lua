b8e18fc9e474978ca8f7db1d4e53ebb340ad759b
Ivan Wang <113294786+Ivan-Wang-J-W@users.noreply.github.com>
2024-05-19 02:06:45 +0800

    added sql plugin dadbod and dadbod ui. left completion to configure

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -68,6 +68,26 @@ require("lazy").setup({
 	'tpope/vim-rhubarb',
 	-- Detect tabstop and shiftwidth automatically
 	'tpope/vim-sleuth',
+	--sql 
+	'tpope/vim-dadbod',
+	--sql completion and ui 
+	{
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+},
 	--vim wiki
 	'vimwiki/vimwiki',
 
